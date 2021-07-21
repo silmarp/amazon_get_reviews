c942a5e19d534ee6a8dd50538eab19b3d3b8f78b
Silmar Junior <silmarjr2@gmail.com>
2021-07-21 11:21:06 -0300

    created and managed items

--- amazon_reviews/items.py
+++ amazon_reviews/items.py
@@ -7,6 +7,6 @@ import scrapy
 
 
 class AmazonReviewsItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    grade =                scrapy.Field()
+    review_title =         scrapy.Field()
+    comment =              scrapy.Field()
--- amazon_reviews/spiders/amazon.py
+++ amazon_reviews/spiders/amazon.py
@@ -1,30 +1,33 @@
 import scrapy
-
+from ..items import AmazonReviewsItem
 
 class AmazonSpider(scrapy.Spider):
-    name = 'amazon'
-    allowed_domains = ['amazon.com']
+    name =                              'amazon'
+    allowed_domains =                   ['amazon.com']
     start_urls = [
             'https://www.amazon.com/Fortnite-7-Llama-Loot-Plush/product-reviews/B07GJ2MWTZ',
     ]
 
 
     def parse(self, response):
+        
+        items = AmazonReviewsItem()
+        
         for review in response.xpath('//div[@data-hook="review"]'):
-            try:
-                yield {
-                    'grade' :            float(review.xpath(".//i[@data-hook='review-star-rating']/span/text()").get()[0:3]),
-                    'review title' :     review.xpath(".//a[@data-hook='review-title']/span/text()").get(),
-                    'comment' :          review.xpath('.//span[@data-hook="review-body"]/span/text()').get(),
-                }
-            except:
-                yield {
-                    'grade' :            0,
-                    'review title' :     "Not Found",
-                    'comment' :          review.xpath('.//span[@data-hook="review-body"]/span/text()').get(),
-                }
+            grade =                     review.xpath(".//i[@data-hook='review-star-rating']/span/text()").get()
+
+            if type(grade) == str:
+                grade =                 float(review.xpath(".//i[@data-hook='review-star-rating']/span/text()").get()[0:3])
+            else:
+                grade =                 "not found"
+
+            items["grade"] =            grade
+            items["review_title"] =     review.xpath(".//a[@data-hook='review-title']/span/text()").get()
+            items["comment"] =          review.xpath('.//span[@data-hook="review-body"]/span/text()').get()
+            
+            yield items 
                 
-        next_page = response.xpath('//li[@class="a-last"]').css('a::attr(href)').get()
+        next_page =                     response.xpath('//li[@class="a-last"]').css('a::attr(href)').get()
 
         if next_page is not None:
             yield response.follow("https://www.amazon.com" + next_page, callback=self.parse)
